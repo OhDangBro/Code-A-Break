d99df56fd6c3e10d330f25ce14cfdcec6ddfedfc
amatos <amatos201@gmail.com>
2022-08-11 11:29:35 -0400

    update & clean code

---
+++ controllers/api/post-routes.js
@@ -0,0 +1,77 @@
+const { Post, Vote } = require("../../models");
+
+router.get('/:id', (req, res) => {
+    Post.findOne({
+      where: {
+        id: req.params.id
+      },
+      attributes: [
+        'id',
+        'post_url',
+        'title',
+        'created_at',
+        [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+      ],
+      include: [
+        {
+          model: Post,
+          attributes: ['id','title','post_url','created_at']
+        },
+    {
+        model: Post,
+        attributes: ['title'],
+        through: Vote,
+        as: 'voted_posts'
+    }  
+    ]
+    })
+      .then(dbPostData => {
+        if (!dbPostData) {
+          res.status(404).json({ message: 'No post found with this id' });
+          return;
+        }
+        res.json(dbPostData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+
+// PUT /api/posts/upvote
+router.put('/upvote', (req, res) => {
+
+});
+
+Vote.create({
+    user_id: req.body.user_id,
+    post_id: req.body.post_id
+
+}).then(() => {
+    // then find the post we just voted on
+    return Post.findOne({
+        where: {
+            id: req.body.post_id
+        },
+        attributes: [
+            'id',
+            'post_url',
+            'title',
+            'created_at',
+            // use raw MySQL aggregate function query to get a count of how many votes the post has and return it under the name 'vote_count'
+            [    sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+            
+            ],
+
+        
+        
+    })
+.then(dbPostData => res.json(dbPostData))
+.catch(err => {
+    console.log(err);
+    res.status(400).json(err);
+});
+
+dbPostData => res.json(dbPostData))
+.catch(err => res.json(err));
