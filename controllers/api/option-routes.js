e01819b8183d0813fde74fde524a65bc84d4fa2b
cmarvann <covingtonmarvann@gmail.com>
2022-08-13 00:16:21 -0400

    create homepage

--- controllers/api/option-routes.js
+++ controllers/api/option-routes.js
@@ -29,12 +29,18 @@ router.get('/', (req, res) => {
       }
     ]
   })
-    .then(dbOptionData => res.json(dbOptionData))
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
+  .then(dbPostData => {
+    console.log(dbPostData[0]);
+    const posts = dbPostData.map(post => post.get({ plain: true }));
+    // res.render('homepage', dbPostData[0].get({ plain: true }));
+    res.render('homepage', { posts });
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
+
 
 router.get('/:id', (req, res) => {
   Option.findOne({
--- server.js
+++ server.js
@@ -1,29 +1,29 @@
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers');
 
-// const path = require('path');
+const path = require('path');
 
-// const exphbs = require('express-handlebars');
+const exphbs = require('express-handlebars');
 
 const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// const hbs = exphbs.create({});
-// app.engine('handlebars', hbs.engine);
-// app.set('view engine', 'handlebars');
+const hbs = exphbs.create({});
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true })); // change to false 811222145pm
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: false})); // change to false 811222145pm
+app.use(express.static(path.join(__dirname, 'public')));
 
-// turn on routes
-app.use(routes);
+// // turn on routes
+// app.use(routes);
 
-// app.use(require('./controllers/'));
+app.use(require('./controllers/'));
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
