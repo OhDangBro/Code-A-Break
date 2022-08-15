337c10967b172d70699983387fcda003969e8d8a
Will <103394466+OhDangBro@users.noreply.github.com>
2022-08-14 22:02:47 -0400

    added food page

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -166,6 +166,50 @@ router.get('/outdoors', (req, res) => {
       res.status(500).json(err);
     });
 });
+
+router.get('/food', (req, res) => {
+  console.log('======================');
+  Post.findAll({
+    where: {
+      options_id: "Food"
+    },
+    attributes: [
+      'id',
+      'post_url',
+      'title',
+      'created_at',
+      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+    .then(dbPostData => {
+      const posts = dbPostData.map(post => post.get({ plain: true }));
+
+      res.render('food', {
+        posts,
+        loggedIn: req.session.loggedIn
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+
 router.get('/study', (req, res) => {
   console.log('======================');
   Post.findAll({
